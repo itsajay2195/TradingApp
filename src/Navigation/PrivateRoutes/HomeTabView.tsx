02676563b03d6dc4931c501eb28c40eb1aca3647
Ajaykumar Rajasekaran <ajayrne@gmail.com>
2025-10-20 19:40:30 +0530

    installed react-query library

--- src/Navigation/PrivateRoutes/HomeTabView.tsx
+++ src/Navigation/PrivateRoutes/HomeTabView.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {Suspense, lazy} from 'react';
 import {
   Animated,
   View,
@@ -8,20 +8,25 @@ import {
 } from 'react-native';
 import {TabView, SceneMap} from 'react-native-tab-view';
 import Icon from '../../components/IconComponent/IconComponent';
-import CoinListScreen from '../../screens/CoinsListScreen/CoinsListScreen';
-import WatchlistScreen from '../../screens/WatchListScreen/WatchlistScreen';
 
-const FirstRoute = () => <CoinListScreen />;
-const SecondRoute = () => <WatchlistScreen />;
+const CoinListScreen = lazy(
+  () => import('../../screens/CoinsListScreen/CoinsListScreen'),
+);
+const WatchlistScreen = lazy(
+  () => import('../../screens/WatchListScreen/WatchlistScreen'),
+);
 
-{
-  /* <Icon
-  library={'Ionicons'}
-  name={'search'}
-  size={20}
-  style={styles.iconStyle}
-/>; */
-}
+const FirstRoute = () => (
+  <Suspense fallback={null}>
+    <CoinListScreen />
+  </Suspense>
+);
+
+const SecondRoute = () => (
+  <Suspense fallback={null}>
+    <WatchlistScreen />
+  </Suspense>
+);
 
 export default class HomeTabView extends React.Component {
   state = {
